c8b4756b4d4a46236574430e4910adda008cf084
Shizuka Kurosawa <gzt.myv0527@gmail.com>
2022-03-21 00:20:42 +0900

    implementation completed

---
+++ src/com/example/Builder.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.List;
+
+public abstract class Builder {
+	
+	public abstract void makeHeader();
+	
+	public abstract void makeEmployeeList(List<Employee> employeeList);
+
+}
