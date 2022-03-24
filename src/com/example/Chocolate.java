c8333636f114b7c769262239735739d3f932d0ce
Shizuka Kurosawa <gzt.myv0527@gmail.com>
2022-03-24 23:06:56 +0900

    implementation completed

---
+++ src/com/example/Chocolate.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Chocolate extends Ice {
+	
+//	チョコアイスの値段
+	private int price = 70;
+	
+	protected Chocolate(IceCream iceCream) {
+		super(iceCream);
+	}
+	
+	@Override
+	public int getPrice() {
+		return price + iceCream.getPrice();
+	}
+	
+	@Override
+	public String getName() {
+		return "チョコ" + iceCream.getName();
+	}
+
+}
---
+++ src/com/example/Ice.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public abstract class Ice extends IceCream {
+	
+//	中身、または装飾のインスタンスを持っている
+	protected IceCream iceCream;
+	
+//	コンストラクタ
+//	中身、または装飾のインスタンスはコンストラクタで指定する
+	protected Ice(IceCream iceCream) {
+		super();
+		this.iceCream = iceCream;
+	}
+	
+}
